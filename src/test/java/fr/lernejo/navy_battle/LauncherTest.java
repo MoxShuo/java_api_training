c6e70a47687698a380c18d0e0707b347558b9f46
Jeremy Vissault <vissault@et.esiea.fr>
2021-12-20 00:32:40 +0100

    Premier Test

---
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -0,0 +1,27 @@
+package fr.lernejo.navy_battle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+
+
+public class LauncherTest {
+    private final Launcher launcher = new Launcher();
+    @Test
+    void mainTest1(){
+        org.assertj.core.api.Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Launcher.main(new String[] {})).withMessage("il doit y avoir 1 ou 2 arguments pour foncitionner");
+    }
+    @Test
+    void mainTest2(){
+        org.assertj.core.api.Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Launcher.main(new String[] {"9786","ok","ok"})).withMessage("il doit y avoir 1 ou 2 arguments");
+    }
+    @Test
+    void mainTest3(){
+        org.assertj.core.api.Assertions.assertThatNoException().isThrownBy(() -> Launcher.main(new String[] {"9786"}));
+    }
+    @Test
+    void mainTest4(){
+        org.assertj.core.api.Assertions.assertThatNoException().isThrownBy(() -> Launcher.main(new String[] {"9786", "http://localhost:9786"}));
+
+    }
+}
