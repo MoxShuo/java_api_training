010bc2d7ade791e7dd205ec97f5db12c73fd2d08
Jeremy Vissault <vissault@et.esiea.fr>
2021-12-20 01:00:02 +0100

    Correction d'erreur Test Launcher

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -9,11 +9,11 @@ public class LauncherTest {
     private final Launcher launcher = new Launcher();
     @Test
     void mainTest1(){
-        org.assertj.core.api.Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Launcher.main(new String[] {})).withMessage("il doit y avoir 1 ou 2 arguments pour foncitionner");
+        org.assertj.core.api.Assertions.assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> Launcher.main(new String[] {})).withMessage("il doit y avoir 1 ou 2 arguments pour foncitionner");
     }
     @Test
     void mainTest2(){
-        org.assertj.core.api.Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Launcher.main(new String[] {"9786","ok","ok"})).withMessage("il doit y avoir 1 ou 2 arguments");
+        org.assertj.core.api.Assertions.assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> Launcher.main(new String[] {"9786","ok","ok"})).withMessage("il doit y avoir 1 ou 2 arguments");
     }
     @Test
     void mainTest3(){
